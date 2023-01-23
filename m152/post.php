4eed58a34271184b323a0ab2699f6d9890b11962
Srijon-pixel <71446391+Srijon-pixel@users.noreply.github.com>
2023-01-23 14:02:47 +0100

    Update post.php

--- m152/post.php
+++ m152/post.php
@@ -1,12 +1,34 @@
 <!DOCTYPE html>
 <html lang="en">
+    <!--
+Projet : Créer un portfolio
+Auteur : Srijon Rahman
+Date : 23.01.23
+Détail : Aucune utilité pour l'instant
+-->
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/base.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
     <title></title>
 </head>
 <body>
-    
+<header>
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container-fluid">
+        <ul class="navbar-nav">
+            <li class="nav-item"><a class="nav-link active" href="./index.php">Home</a></li>
+            <li class="nav-item"><a class="nav-link active" href="#">Post</a></li>
+        </ul>
+    </div>
+</nav>
+
+
+</header>
+<main></main>
+<footer></footer>
 </body>
 </html>
