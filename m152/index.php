eb6aa40b5d3c2262e5166a81a7b46a0e70341d3f
Srijon-pixel <71446391+Srijon-pixel@users.noreply.github.com>
2023-01-23 13:50:15 +0100

    Create index.php

---
+++ m152/index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/base.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+    <title>Home</title>
+</head>
+
+<body>
+    <?php
+
+
+    ?>
+    <header>
+
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div class="container-fluid">
+                <ul class="navbar-nav">
+                    <li class="nav-item"><a class="nav-link active" href="#">Home</a></li>
+                    <li class="nav-item"><a class="nav-link active" href="./post.php">Post</a></li>
+                </ul>
+            </div>
+        </nav>
+    </header>
+    <main>
+<div class="card">
+    <img src="./img/photo_profil.jpg" alt="photo">
+    <div class="container">
+    <p>Bienvenu sur mon portfolio</p>
+    <p>Nom : Srijon Rahman</p>
+    <p>Passion : jouer au jeux vidéo</p>
+    </div>
+</div>
+
+    </main>
+    <footer></footer>
+
+</body>
+
+</html>
