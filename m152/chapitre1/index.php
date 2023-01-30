ec8ac6fd41813c304df8f70903eb7d9231356f82
Srijon-pixel <71446391+Srijon-pixel@users.noreply.github.com>
2023-01-30 16:04:02 +0100

    Update and rename ad to index.php

---
+++ m152/chapitre1/index.php
@@ -0,0 +1,54 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<!--
+Projet : Créer un portfolio
+Auteur : Srijon Rahman
+Date : 23.01.23
+Détail : Page d'accueil du site
+-->
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/base.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+  
+    <title>Home</title>
+</head>
+
+<body>
+    <?php
+
+
+    ?>
+    <header>
+
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div class="container-fluid">
+                <ul class="navbar-nav">
+                    <li class="nav-item" class="bi bi-house"><a class="nav-link active" href="#">Home</a></li>
+                    <li class="nav-item"><a class="nav-link active" href="./post.php">Post</a></li>
+                    <li class="nav-item"><a class="nav-link active" href="#">Login</a></li>
+                </ul>
+            </div>
+        </nav>
+        <h1>Bienvenu sur mon portfolio</h1>
+
+    </header>
+    <main>
+        <div class="card">
+            <img src="./img/photo_profil.jpg" alt="photo">
+            <div class="container">
+                <p>Nom : Srijon Rahman</p>
+                <p>Passion : Jouer aux jeux vidéo</p>
+            </div>
+        </div>
+
+    </main>
+    <footer></footer>
+
+</body>
+
+</html>
