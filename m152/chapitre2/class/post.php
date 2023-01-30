8d5d20c4a3613e7198756a169e557ec6e74953ec
Srijon-pixel <71446391+Srijon-pixel@users.noreply.github.com>
2023-01-30 16:08:08 +0100

    Add files via upload

---
+++ m152/chapitre2/class/post.php
@@ -0,0 +1,47 @@
+<?php
+
+/**
+ * Classe container post
+ */
+class EPost
+{
+
+    /**
+     * Constructeur permettant de créer un nouveau poste
+     * @param integer $InIdBudget L'identifiant du budget
+     * @param string $InNom le nom du budget
+     * @param integer $InMontant le montant du budget
+     */
+    public function __construct($InIdPost, $InDescription = "", $InDateCreation = "", $InDateModification = "", $InIdMedia)
+    {
+
+        $this->idPost = $InIdPost;
+        $this->description = $InDescription;
+        $this->dateCreation = $InDateCreation;
+        $this->dateModification = $InDateModification;
+        $this->idMedia = $InIdMedia;
+    }
+
+
+
+    /**
+     * @var integer identifiant du poste
+     */
+    public $idPost;
+    /**
+     * @var string description du poste
+     */
+    public $description;
+    /**
+     * @var string date de création du poste
+     */
+    public $dateCreation;
+    /**
+     * @var string date de modification du poste
+     */
+    public $dateModification;
+    /**
+     * @var int identifiant du média
+     */
+    public $idMedia;
+}
